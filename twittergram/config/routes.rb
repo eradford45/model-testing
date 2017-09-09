bf869ed32010a85daf5300285dc1f114815fe667
Erik McComb <erik@erikmccomb.com>
2017-09-09 10:36:26 -0600

    controllers added

--- twittergram/config/routes.rb
+++ twittergram/config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/show'
+
+  get 'comments/edit'
+
+  get 'bios/index'
+
+  get 'bios/show'
+
+  get 'bios/edit'
+
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  devise_for :views
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
