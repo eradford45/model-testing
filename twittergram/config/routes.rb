b7915ec846a34c356ebf177b98c12df66bbc21e5
Erik McComb <erik@erikmccomb.com>
2017-09-09 10:56:07 -0600

    corrected routes

--- twittergram/config/routes.rb
+++ twittergram/config/routes.rb
@@ -1,29 +1,6 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-
-  get 'comments/show'
-
-  get 'comments/edit'
-
-  get 'bios/index'
-
-  get 'bios/show'
-
-  get 'bios/edit'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/edit'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  devise_for :views
+  root to: 'accounts#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :accounts
 end
