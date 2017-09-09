6f332bfefe9b9c052f5c31eca0fdd330b7a8f114
Erik McComb <erik@erikmccomb.com>
2017-09-09 11:47:48 -0600

    models updated

--- twittergram/app/models/user.rb
+++ twittergram/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_one :bio, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
 end
